ff5a29c56e3c99334b13ff064336275939178f14
senichek <senichek@gmail.com>
2022-10-25 21:45:18 +0200

    different types of messages

--- src/components/chatUser/index.js
+++ src/components/chatUser/index.js
@@ -2,7 +2,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setInputMsg } from '../../store/actions';
 import uuid from 'react-uuid';
 import { ThreeDots } from  'react-loader-spinner'
-//import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { useRef, useEffect } from 'react';
+import './style.scss';
+import { dateFormatter } from '../../utils/dateFormatter';
+
 
 const UserChat = ({ sendMsg }) => {
 
@@ -10,6 +13,20 @@ const UserChat = ({ sendMsg }) => {
     const messages = useSelector((state) => state.user.messages);
     const helperConnected = useSelector((state) => state.user.helperConnected);
     const helperJoinedYourChat = useSelector((state) => state.user.helperJoinedYourChat);
+    const loggedInUser = useSelector((state) => state.user);
+
+    const messageRef = useRef(null);
+
+    // Scroll the list of messages to the last message automatically.
+    useEffect(() => {
+        if (messageRef.current) {
+            messageRef.current.scrollIntoView(
+              {
+                behavior: 'smooth',
+                block: 'end',
+              })
+          }
+    }, [messages])
 
     const dispatch = useDispatch();
 
@@ -21,7 +38,8 @@ const UserChat = ({ sendMsg }) => {
         const message = {
             id: uuid(),
             text: inputMsg,
-            date: new Date()
+            date: new Date(),
+            author: loggedInUser.id
         }
         sendMsg(message);
     }
@@ -29,7 +47,7 @@ const UserChat = ({ sendMsg }) => {
 
  return (
     <>
-        {!helperConnected && <div className="user-chat__no_helpers">Chat is unavailable because there are no helpers online, sorry about that.</div>}
+        {!helperConnected && <div className="user-chat__no_helpers">Chat is unavailable because the helpers are offline, sorry about that.</div>}
         {(!helperJoinedYourChat && helperConnected) && 
         (
             <>
@@ -54,9 +72,13 @@ const UserChat = ({ sendMsg }) => {
         {(helperJoinedYourChat && helperConnected) &&
                 <div className="user-chat">
                     <div className="user-chat__messages">
-                        {messages.map(msg => (<div className="user-chat__single_msg" key={msg.id}>
-                        {msg.text}
-                        </div>))}
+                        {messages.map(msg => (
+                        <div className={loggedInUser.id !== msg.author ? 'user-chat__single_msg_not_yours' : 'user-chat__single_msg'} key={msg.id} ref={messageRef}>
+                            <div className="user-chat__single_msg_author">From: {msg.author}</div>
+                            <div className="user-chat__single_msg_txt">{msg.text}</div>
+                            <div className="user-chat__single_msg_date">{dateFormatter(msg.date)}</div>
+                        </div>
+                        ))}
                     </div>
                         <textarea className="user-chat__text_input" rows="5" cols="33" onChange={handleInputChange} defaultValue={inputMsg}></textarea>
                         <button className="user-chat__send_btn" onClick={handleSendMsg}>Send</button>
