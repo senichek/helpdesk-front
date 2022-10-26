c1b96f2fa8c0c27e58759495a1b14f26c0f1cdbb
senichek <senichek@gmail.com>
2022-10-26 16:02:24 +0200

    Differentiate between chats of diff users

--- src/components/chatHelper/index.js
+++ src/components/chatHelper/index.js
@@ -14,6 +14,10 @@ const HelperChat = ({ sendMsg }) => {
 
     const messageRef = useRef(null);
 
+    // By the default the recipient = ID of the logged-in user. All the
+    // users join their own room by default (room = userId)
+    const recipient = useSelector((state) => state.user.recipient);
+
     // Scroll the list of messages to the last message automatically.
     useEffect(() => {
         if (messageRef.current) {
@@ -40,9 +44,21 @@ const HelperChat = ({ sendMsg }) => {
             id: uuid(),
             text: inputMsg,
             date: new Date(),
-            author: loggedInUser.id
+            author: loggedInUser.id,
+            // Chat always belongs to the user (not to the helper).
+            // If you are logged in as 'helper' the chatID = recipient.
+            // If you are logged in as @user' the chatID = loggedInUser.id.
+            chatId: recipient
         }
         sendMsg(message);
+        dispatch(setInputMsg(''));
+    }
+
+    const onEnterPress = (event) => {
+        if(event.keyCode === 13 && event.shiftKey === false) {
+            event.preventDefault();
+            handleSendMsg();
+        }
     }
 
     return (
@@ -54,16 +70,18 @@ const HelperChat = ({ sendMsg }) => {
                 ))}
             </div>
             <div className="helper-chat__working_zone">
+                {recipient !== loggedInUser.id && <div>You are chatting with {chatUsers.find(el => el.userId === recipient).nickname}</div>}
             <div className="helper-chat__messages">
-                {messages.map(msg => (
+            {/* options.filter(opt => !opt.assigned).map(opt => someNewObject) */}
+                {messages.filter(msg => msg.chatId === recipient).map(msg => (
                 <div className={loggedInUser.id !== msg.author ? 'helper-chat__single_msg_not_yours' : 'helper-chat__single_msg'} key={msg.id} ref={messageRef}>
-                    <div className="helper-chat__single_msg_author">From: {msg.author}</div>
+                    <div className="helper-chat__single_msg_author">From: {chatUsers.find(el => el.userId === msg.author).nickname}</div>
                     <div className="helper-chat__single_msg_txt">{msg.text}</div>
                     <div className="helper-chat__single_msg_date">{dateFormatter(msg.date)}</div>
                 </div>
                 ))}
             </div>
-            <textarea className="helper-chat__text_input" rows="5" cols="33" onChange={handleInputChange} defaultValue={inputMsg}></textarea>
+            <textarea className="helper-chat__text_input" rows="5" cols="33" onChange={handleInputChange} value={inputMsg} onKeyDown={onEnterPress}></textarea>
             <button className="helper-chat__send_btn" onClick={handleSendMsg}>Send</button>
             </div>
         </div>
