3d51146c0d8cff8adb9444f2cc654d3ec1195f89
senichek <senichek@gmail.com>
2022-11-01 21:06:19 +0100

    small adjustment

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,5 +1,6 @@
 import './reset.scss';
 import './style.scss';
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
 
@@ -12,11 +13,11 @@ const Welcome = () => {
         <div className="intro-container">
             <div className="intro">
                 <h1 className="intro__header">Help Desk Application</h1>
-                <p className="intro__text">I did this project in order to experiment with Socket.IO a bit. This is a simple chat app where users 
+                <p className="intro__text">I did this project in order to experiment a bit with Socket.IO. This is a simple chat app where users 
                 can register as "helpers" if they want to provide help to others (act as tech assistants or tech support) or as "user" if 
                 they'd like to get some help.</p>
                 <div className="intro__links">
-                <a className="intro__link" href="/login">Run the app {'>>'}</a>
+                <Link to="/login" className="intro__link">Run the app {'>>'}</Link>
                 <a className="intro__link" href="https://github.com/senichek/helpdesk-front">Front repo {'>>'}</a>
                 <a className="intro__link" href="https://github.com/senichek/helpdesk-back">Back repo {'>>'}</a>
                 </div>
